fa826b3466145fd5f7e128492074b31fe2c03519
John Dailey <johnjdailey@gmail.com>
2020-11-05 23:58:43 -0500

    Complete Day 4

--- applications/crack_caesar/crack_caesar.py
+++ applications/crack_caesar/crack_caesar.py
@@ -1,5 +1,70 @@
+#crack_caesar.py
+
+
+
 # Use frequency analysis to find the key to ciphertext.txt, and then
 # decode it.
 
-# Your code here
 
+import re
+from collections import defaultdict
+
+
+frequencies = """
+|   E    |    11.53   |
+|   T    |     9.75   |
+|   A    |     8.46   |
+|   O    |     8.08   |
+|   H    |     7.71   |
+|   N    |     6.73   |
+|   R    |     6.29   |
+|   I    |     5.84   |
+|   S    |     5.56   |
+|   D    |     4.74   |
+|   L    |     3.92   |
+|   W    |     3.08   |
+|   U    |     2.59   |
+|   G    |     2.48   |
+|   F    |     2.42   |
+|   B    |     2.19   |
+|   M    |     2.18   |
+|   Y    |     2.02   |
+|   C    |     1.58   |
+|   P    |     1.08   |
+|   K    |     0.84   |
+|   V    |     0.59   |
+|   Q    |     0.17   |
+|   J    |     0.07   |
+|   X    |     0.07   |
+|   Z    |     0.03   |
+"""
+
+
+letter_to_freq = {}
+for row in frequencies.split("\n"):
+    m = re.match(r"\|\s+(?P<letter>[A-Z])\s+\|\s+(?P<number>\d+\.\d+)\s+|", row)
+    if m and m.group("letter") and m.group("number"):
+        letter_to_freq[m.group("letter")] = float(m.group("number"))/100.
+
+freq_to_letter = {v: k for k, v in letter_to_freq.items()}
+
+with open("applications\crack_caesar\ciphertext.txt") as f:
+    text = f.read()
+
+
+letter_counts = defaultdict(lambda: 0)
+for letter in text:
+    letter = letter.upper()
+    if 'A' <= letter and letter <= 'Z':
+        letter_counts[letter] += 1
+
+def get_closest(freq):
+    return min(freq_to_letter.keys(), key=lambda x: abs(x-freq))
+
+total = sum(x for x in letter_counts.values())
+for letter, count in letter_counts.items():
+    freq = count/total
+    closest = freq_to_letter[get_closest(freq)].lower()
+    text = text.replace(letter, closest)
+
+print(text)
--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -1,13 +1,40 @@
+#markov.py
+
+
+
 import random
+from collections import defaultdict
+from spacy.lang.en import English
+from spacy.tokenizer import Tokenizer
+
 
 # Read in all the words in one go
-with open("input.txt") as f:
+with open("applications\markov\input.txt") as f:
     words = f.read()
 
+
 # TODO: analyze which words can follow other words
-# Your code here
+
+nlp = English()
+tokenizer = Tokenizer(nlp.vocab)
+
+w2w = defaultdict(lambda: [])
+words = [re.sub(r"[^a-z0-9]", "", t.lemma_.lower()).strip() for t in tokenizer(words)
+         if not t.is_punct and t.text.strip()]
+for word1, word2 in zip(words, words[1:]):
+    w2w[word1].append(word2)
 
 
 # TODO: construct 5 random sentences
-# Your code here
 
+for _ in range(5):
+    word = random.choice(list(w2w.keys()))
+    sentence = [word]
+    for _ in range(random.randint(4, 12)):
+        following = w2w[word]
+        if following:
+            word = random.choice(following)
+            sentence.append(word)
+        else:
+            break
+    print(" ".join(sentence))
